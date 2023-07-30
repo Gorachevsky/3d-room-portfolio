37e1e930f809ae16ff43fbc1c53c923f4f93f140
Gorachevsky <anton.gorachevskiy@gmail.com>
2023-07-30 16:23:48 +0200

    Resize Cameras
    Added resize method for all type of cameras in Camera.js

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -34,4 +34,19 @@ export default class Camera {
     );
     this.scene.add(this.orthographicCamera);
   }
+
+  resize() {
+    // Updating Perspective Camera on Resize
+    this.perspectiveCamera.aspect = this.sizes.aspect;
+    this.perspectiveCamera.updateProjectionMatrix();
+
+    // Updating Orthographic Camera on Resize
+    this.orthographicCamera.left =
+      (-this.sizes.aspect * this.sizes.frustrum) / 2;
+    this.orthographicCamera.right =
+      (this.sizes.aspect * this.sizes.frustrum) / 2;
+    this.orthographicCamera.top = this.frustrum / 2;
+    this.orthographicCamera.bottom = -this.frustrum / 2;
+    this.orthographicCamera.updateProjectionMatrix();
+  }
 }
