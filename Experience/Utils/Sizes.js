f922f04f3baacbecda4db57319127515c8a3b7d4
Gorachevsky <anton.gorachevskiy@gmail.com>
2023-07-30 17:13:52 +0200

    Fix Sizes constructor
    Added super() method to avoid exceptions on EventEmitter execution

--- Experience/Utils/Sizes.js
+++ Experience/Utils/Sizes.js
@@ -2,6 +2,7 @@ import { EventEmitter } from "events";
 
 export default class Sizes extends EventEmitter {
   constructor() {
+    super();
     this.width = window.innerWidth;
     this.height = window.innerHeight;
     this.aspect = this.width / this.height;
